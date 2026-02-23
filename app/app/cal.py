67865df79b9695cd00c3d07b2988c1b0e11e6534
Roji <kmahapatra@theventurestudio.co>
2026-02-23 19:30:58 +0530

    flake 8 rule  blankline

--- app/app/cal.py
+++ app/app/cal.py
@@ -2,11 +2,13 @@
 calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """add x and y and return result"""
     return x + y
 
-def substract(x,y):
+
+def substract(x, y):
     """ substract x from y and return the result """
 
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,17 +6,15 @@ from django.test import SimpleTestCase
 
 from app import cal
 
+
 class CalcTests(SimpleTestCase):
     """ test the calc module """
     def test_add_numbers(self):
         """ test adding numbers together """
         res = cal.add(3, 4)
-
-        self.assertEqual(res,7)
+        self.assertEqual(res, 7)
 
     def test_substract_number(self):
         """ test substracting numbers """
         res = cal.substract(10, 15)
-
-        self.assertEqual(res,5)
-
+        self.assertEqual(res, 5)
